0927df532f8b43724143e98f93b42d65c32222a7
alexgurrola <alex.gurrola@versidyne.com>
2014-06-20 16:37:03 -0700

    Initial commit

---
+++ Banner/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Banner")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Versidyne LLC")> 
+<Assembly: AssemblyProduct("Banner")> 
+<Assembly: AssemblyCopyright("Copyright © Versidyne LLC 2010")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("66ba66f2-1c2f-4bd2-bb0a-653a18240a27")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
